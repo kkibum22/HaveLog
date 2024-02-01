2810047d114ca6f2db57e984c4833c3d26b566ad
kkibum22 <950610@naver.com>
2024-02-02 00:17:20 +0900

    ✨ Express,TypeScript 환경 설정 작업

---
+++ src/app.ts
@@ -0,0 +1,17 @@
+import express, { Request, Response, urlencoded } from 'express';
+import cors from 'cors';
+import helmet from 'helmet';
+
+const app: express.Application = express();
+
+app.use(cors());
+app.use(helmet());
+app.use(urlencoded({ extended: true }));
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('Hello World');
+});
+
+app.listen(8000, () => {
+  console.log('서버가 실행되었습니다.');
+});
